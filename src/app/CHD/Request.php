78ce553d45b3d54e6faf77feef4f1c17545a2737
Thierry Degeling <thierry@syn2cat.lu>
2016-11-26 18:53:31 +0100

    caching

--- src/app/CHD/Request.php
+++ src/app/CHD/Request.php
@@ -4,14 +4,22 @@ namespace App\CHD;
 
 class Request
 {
+    protected $data;
+
     public function get($url)
     {
+        if (isset($this->data[$url])) {
+            return $this->data[$url];
+        }
+
         //$this->app['log']->debug('getting page:', array('url' => $url));
         //echo 'getting page:'.$url;
         $data = file_get_contents($url);
         //$this->app['log']->info('loaded page: ', array('url' => $url));
         //echo 'loaded page: '.$url;
 
-        return trim(preg_replace('/\s+/', ' ', $data));
+        $this->data[$url] = trim(preg_replace('/\s+/', ' ', $data));
+
+        return $this->data[$url];
     }
 }
