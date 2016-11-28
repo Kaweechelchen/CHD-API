d39d37878159dda2892126f22cce5bbfaf696932
Thierry Degeling <thierry@syn2cat.lu>
2016-11-28 13:32:48 +0100

    new view, trying to get statuses

--- src/app/Http/Controllers/PetitionController.php
+++ src/app/Http/Controllers/PetitionController.php
@@ -4,12 +4,37 @@ namespace App\Http\Controllers;
 
 use App\Petition;
 use App\Status;
+use Illuminate\Support\Facades\DB;
 
 class PetitionController extends Controller
 {
     public function index()
     {
-        $petitions = Petition::all();
+        //$petitions = Petition::all();
+        $petitions = Petition::orderBy('submission_date', 'desc')
+            ->limit(3)
+            ->offset(19)
+            ->get();
+
+        /*DB::listen(function ($query) {
+            dd($query->sql, $query->bindings, $query->time);
+        });*/
+
+        /*$petitions = Petition::join('petition_statuses', 'petitions.id', '=', 'petition_statuses.petition_id')
+                             ->join('statuses',          'statuses.id',  '=', 'petition_statuses.status_id')
+                             ->selectRaw('petitions.id, MAX(petition_statuses.created_at)')
+                             //->latest('statuses.created_at')
+                             //->havingRaw('MAX(petition_statuses.created_at) = petition_statuses.created_at')
+                             //->latest('petition_statuses.created_at')
+                             //->groupBy('petitions.id')
+                             //->orderBy('petitions.created_at', 'desc')
+                             //->where('petitions.id', 29)
+                             ->limit(3)
+                             //->offset(16)
+                             ->get();*/
+
+        //return $petitions;
+
         $status    = Status::all();
 
         return view('petitions', compact('petitions', 'status'));
--- src/resources/views/petitions.blade.php
+++ src/resources/views/petitions.blade.php
@@ -2,7 +2,30 @@
 
 @section('content')
     @foreach ($petitions as $petition)
+
         <div class="row well">
+            <div class="col-xs-2">
+                <h1>{{ $petition->number }}</h1>
+                {{ $status->find($petition->statuses()->orderBy('created_at', 'desc')->first()->status_id)->status }}
+            </div>
+            <div class="col-xs-8">
+                <div class="row">
+                    <h3>
+                        {{ $petition->description }} &middot;
+                        <small>
+                            @foreach($petition->authors as $author)
+                                {{ $author->name }}
+                            @endforeach
+                        </small>
+                    </h3>
+                </div>
+            </div>
+            <div class="col-xs-2">
+                {{ count($petition->signatures) }} / 4500 ({{ round((count($petition->signatures) / 4500 * 100), 1) }}%)
+            </div>
+        </div>
+
+        {{--<div class="row well">
             <h4>{{ $petition->name }} | {{ $status->find($petition->statuses()->orderBy('created_at', 'desc')->first()->status_id)->status }} &middot;
             <small>
                 @foreach($petition->authors as $author)
@@ -21,6 +44,6 @@
             <div class="col-sm-6">
                 {{ count($petition->signatures) }}
             </div>
-        </div>
+        </div>--}}
     @endforeach
 @stop
