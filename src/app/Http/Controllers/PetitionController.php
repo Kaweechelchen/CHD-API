dbcaea4bd3446351d590d7256d0f4d9740b324cb
Thierry Degeling <thierry@syn2cat.lu>
2016-12-03 15:24:46 +0100

    cleanup

--- src/app/Http/Controllers/PetitionController.php
+++ src/app/Http/Controllers/PetitionController.php
@@ -4,39 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Petition;
 use App\Status;
-use App\Signature;
 use App\SignatureStats;
 
 class PetitionController extends Controller
 {
     public function index()
     {
-        //app('Stats')->init();
-
-        //return app('Stats')->hours();
-
-        //return app('Stats')->days();
-
-        //return app('Stats')->petitionSignaturesByDay(807, 4);
-
         $petitions = Petition::includingStatus(20);
         $petitions = Petition::withStatus(4, 200);
-
-        //$petitions = Petition::all();
-        /*$petitions = Petition::orderBy('number', 'desc')
-            ->limit(20)
-            ->get();*/
-
-        /*$count = Signature::whereBetween(
-            'created_at', [
-                date('Y-m-d H:i:s', strtotime('-4 days')),
-                date('Y-m-d H:i:s'),
-            ]
-        )->count();*/
-
-        //dd($count);
-
-        //dd();
 
         $weeklyStats = SignatureStats::where('scope', 'global')
             ->where('unit', 'hour')
@@ -46,7 +21,6 @@ class PetitionController extends Controller
         return view('petitions', compact('petitions', 'weeklyStats'));
     }
 
-    //public function show(Petition $petition)
     public function show($petition)
     {
         $status    = Status::all();
