cef454ba0fbefa88b2566f36b3a4b85a3992450b
Thierry Degeling <thierry@syn2cat.lu>
2016-12-03 18:22:17 +0100

    not versionning untl deploy

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -15,8 +15,8 @@ require('laravel-elixir-vue-2');
 
 elixir((mix) => {
     mix.sass('app.scss')
-        .webpack('app.js')
-        .version(['css/app.css', 'js/app.js']);
+        .webpack('app.js');
+//        .version(['css/app.css', 'js/app.js']);
 
     mix.copy('node_modules/font-awesome/fonts', 'public/build/fonts');
 });
