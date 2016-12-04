eff684bc35923ae2acefa628636ceb0d78f71c2b
Thierry Degeling <thierry@syn2cat.lu>
2016-12-04 12:39:19 +0100

    responsive

--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -2,6 +2,9 @@
 <html lang="en">
 
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Mona Petitions @yield('title')</title>
     <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
 </head>
