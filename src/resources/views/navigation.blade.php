abf5602deac7b0af1e393f3ab16d254a446914f2
Thierry Degeling <thierry@syn2cat.lu>
2016-12-04 18:07:01 +0100

    responsive navigation

--- src/resources/views/navigation.blade.php
+++ src/resources/views/navigation.blade.php
@@ -1,20 +1,21 @@
 <nav class="navbar navbar-light bg-faded navbar-static-top">
-    <div class="container">
-    <a class="navbar-brand" href="/">Petitions.lu</a>
-    <ul class="nav navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" href="/">List</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/about">About</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link">API(soon<sup><i class="fa fa-trademark" aria-hidden="true"></i></sup>)</a>
-        </li>
-    </ul>
-    <form class="form-inline float-xs-right">
-        <input class="form-control" disabled="disabled" type="text" placeholder='Search (soon™) 😋'>
-        {{--<button class="btn btn-outline-success" type="submit">Search</button>--}}
-    </form>
+    <button class="navbar-toggler hidden-md-up" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"></button>
+    <div class="collapse navbar-toggleable-sm" id="navbarResponsive">
+        <a class="navbar-brand" href="/">Petitions.lu</a>
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="/">List</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link">API(soon<sup><i class="fa fa-trademark" aria-hidden="true"></i></sup>)</a>
+            </li>
+        </ul>
+        <form class="form-inline float-xs-right">
+            <input class="form-control" disabled="disabled" type="text" placeholder='Search (soon™) 😋'>
+            {{--<button class="btn btn-outline-success" type="submit">Search</button>--}}
+        </form>
     </div>
 </nav>
