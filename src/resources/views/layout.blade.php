512d9daf29fd4cb80893043872d67526b9047f10
Thierry Degeling <thierry@syn2cat.lu>
2016-12-04 18:17:14 +0100

    Title

--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Mona Petitions @yield('title')</title>
+    <title>Petitions.lu @yield('title')</title>
     <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
 </head>
 
