53bdccc79f60784360ced7178aecae9e854eb098
Thierry Degeling <thierry@syn2cat.lu>
2016-12-05 18:36:54 +0100

    retrying failed connections

--- src/app/CHD/Request.php
+++ src/app/CHD/Request.php
@@ -3,6 +3,8 @@
 namespace App\CHD;
 
 use Illuminate\Support\Facades\Log;
+use ErrorException;
+use Exception;
 
 class Request
 {
@@ -16,13 +18,37 @@ class Request
 
         Log::info('request started for:  '.$url);
 
-        $data = file_get_contents($url);
-        //$this->app['log']->info('loaded page: ', array('url' => $url));
+        $fetched = false;
+        $retry   = 1;
+
+        while ($retry <= 5 && !$fetched) {
+            $data = $this->fetch($url);
+            if ($data) {
+                $fetched = true;
+            } else {
+                sleep(10);
+                Log::error('retry '.$retry.' for: '.$url);
+                ++$retry;
+            }
+        }
+
+        if (!$fetched) {
+            throw new Exception('couldn\'t fetch: '.$url);
+        }
 
         Log::info('request finished for: '.$url);
 
         $this->data[$url] = trim(preg_replace('/\s+/', ' ', $data));
 
         return $this->data[$url];
+    }
+
+    protected function fetch($url)
+    {
+        try {
+            return file_get_contents($url);
+        } catch (ErrorException $e) {
+            return false;
+        }
     }
 }
